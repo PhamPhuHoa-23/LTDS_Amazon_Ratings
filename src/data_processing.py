5a76fca29bf03c3ca2225f199044482596c70d39
Phạm Phú Hòa <phamhoa23us@gmail.com>
2025-11-16 00:10:02 +0700

    Add detect_missing_values function

---
+++ src/data_processing.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def detect_missing_values(data):
+    """
+    Detect missing, null, NaN values in data.
+    
+    Returns:
+        Boolean array indicating missing positions
+    """
+    if data.dtype.names:
+        missing_mask = {}
+        for field in data.dtype.names:
+            if np.issubdtype(data[field].dtype, np.number):
+                missing_mask[field] = np.isnan(data[field])
+            else:
+                missing_mask[field] = (data[field] == b'') | (data[field] == '')
+        return missing_mask
+    else:
+        if np.issubdtype(data.dtype, np.number):
+            return np.isnan(data)
+        else:
+            return (data == b'') | (data == '')
+
